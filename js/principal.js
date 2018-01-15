a9f6feb3b093618b4cf393b2857cd0d9cd9bb6a0
Eric de Olivera Rodrigues <eric.erikito@gmail.com>
2018-01-15 01:10:39 -0200

    Variáveis e Operadores

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,19 @@
 
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+
+var imc = paciente.querySelector(".info-imc");
+
+if (peso <= 0 ||peso >= 1000 ) {
+    imc.textContent = "Peso inválido";
+}else if ( altura < 0 || altura > 3.00 ){
+    imc.textContent = "Altura inválida";
+}else{
+    imc.textContent = peso / (altura * altura);
+}
+
+
